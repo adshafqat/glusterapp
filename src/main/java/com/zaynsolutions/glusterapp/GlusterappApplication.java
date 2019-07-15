e632ab0cd414b21a78a6472dd1ab545bd6f3f6a0
Adeel Shafqat <adeelshafqat@adeels-mbp.home>
2019-07-15 23:17:56 +0100

    code change

--- src/main/java/com/zaynsolutions/glusterapp/GlusterappApplication.java
+++ src/main/java/com/zaynsolutions/glusterapp/GlusterappApplication.java
@@ -3,7 +3,8 @@ package com.zaynsolutions.glusterapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {"com.zaynsolutions.glusterapp"})
 public class GlusterappApplication {
 
 	public static void main(String[] args) {
