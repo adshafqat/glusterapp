6765a0ef29ad7d1a1d3a3f1f12f2431a233978c7
Adeel Shafqat <adeelshafqat@Adeels-MacBook-Pro.local>
2019-07-16 09:38:09 +0100

    Shipley code

--- src/main/java/com/zaynsolutions/glusterapp/GlusterApp.java
+++ src/main/java/com/zaynsolutions/glusterapp/GlusterApp.java
@@ -11,28 +11,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 //Spring Boot 2.x
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //Spring Boot 1.x
 //import org.springframework.boot.web.support.SpringBootServletInitializer;
  
-@SpringBootApplication
-public class GlusterApp extends SpringBootServletInitializer {
- 
-   public static void main(String[] args) {
-       SpringApplication.run(applicationClass, args);
-   }
- 
-   @Override
-   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-       return application.sources(applicationClass);
-   }
- 
-   private static Class<GlusterApp> applicationClass = GlusterApp.class;
-}
- 
+
 @RestController
-class HelloController {
+class GlusterApp {
  
    @RequestMapping("/hello/{name}")
    String hello(@PathVariable String name) {
